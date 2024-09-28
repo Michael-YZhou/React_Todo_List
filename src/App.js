4c4fb2b3c4f388e1198bc4eb6fa1f60c6e7bc21e
Yang Zhou <yangzhouinter@gmail.com>
2024-09-28 17:25:47 +1000

    dynamically display todo items in the todo list

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 class App extends Component {
+  // initial state
+  state = {
+    todos: [
+      { id: "001", name: "Eat", done: true },
+      { id: "002", name: "Sleep", done: true },
+      { id: "003", name: "Code", done: false },
+    ],
+  };
+
   render() {
+    const { todos } = this.state;
+
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header />
-          <List />
+          <List todos={todos} />
           <Footer />
         </div>
       </div>
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -3,11 +3,12 @@ import "./index.css";
 
 export default class Item extends Component {
   render() {
+    const { name, done } = this.props;
     return (
       <li>
         <label>
-          <input type="checkbox" />
-          <span>xxxx</span>
+          <input type="checkbox" defaultChecked={done} />
+          <span>{name}</span>
         </label>
         <button className="btn btn-danger" style={{ display: "none" }}>
           Delete
