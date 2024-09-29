47b1b9056b6ed7184d4637676b64f5f555a8b005
Yang Zhou <yangzhouinter@gmail.com>
2024-09-29 21:41:59 +1000

    apply checking for prop types

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Item from "../Item";
 import "./index.css";
 
 export default class List extends Component {
+  // check data type of props
+  static propTypes = {
+    todos: PropTypes.array.isRequired,
+    updateTodo: PropTypes.func.isRequired,
+  };
+
   render() {
     const { todos, updateTodo } = this.props;
     return (
